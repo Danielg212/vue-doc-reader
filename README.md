e3214a03a816edb344ab41430a8cf6ec4b592f7e
daniel212 <blaaarp@gmail.com>
2020-09-15 01:23:15 +0300

    first commit

---
+++ README.md
@@ -0,0 +1,45 @@
+<h1 align="center">Welcome to vue-doc-reader 👋</h1>
+<p>
+  <img alt="Version" src="https://img.shields.io/badge/version-0.1.0-blue.svg?cacheSeconds=2592000" />
+  <img src="https://img.shields.io/badge/node-%3E%3D10-blue.svg" />
+  <a href="#" target="_blank">
+    <img alt="License: MIT" src="https://img.shields.io/badge/License-MIT-yellow.svg" />
+  </a>
+</p>
+
+> file input reader for docs
+
+## Prerequisites
+
+- node >=10
+
+## Install
+
+```sh
+npm i vue-doc-reader
+```
+
+## Usage
+
+```ts
+import VueDocReader from 'vue-doc-reader'
+vue.component(VueDocReader)
+```
+in parent component
+```html
+ <vue-doc-reader @onLoad="onLoad" />
+```
+
+
+
+## Author
+
+👤 **daniel212**
+
+
+## Show your support
+
+Give a ⭐️ if this project helped you!
+
+***
+_This README was generated with ❤️ by [readme-md-generator](https://github.com/kefranabg/readme-md-generator)_
---
+++ src/vue-doc-reader.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="vue-doc-reader"> 
+     <label for="file">Spreadsheet</label>
+     <input type="file" class="form-control" id="file" :accept="SheetJSFT" @change.prevent="readFile($event)" />
+  </div>
+</template>
+<script lang="ts">
+  import Vue from 'vue';
+  import Component from 'vue-class-component';
+  import * as XLSX from 'xlsx';
+
+  @Component({})
+  export default class VueDocReaderComponent extends Vue {
+
+  private data:Array<any> = new Array<any>()
+  private headers:Array<string> = new Array<string>()
+
+
+  SheetJSFT = [
+    "xlsx", "xlsb", "xlsm", "xls", "xml", "csv", "txt", "ods", "fods", "uos", "sylk", "dif", "dbf", "prn", "qpw", "123", "wb*", "wq*", "html", "htm"
+  ].map(function(x) { return "." + x; }).join(",");
+
+  make_cols = (refstr:Array<string>) => refstr.map((item:any)=> {return {text:item,value:item}})
+
+  get mimeTypes(){
+    return ["text/csv", "text/x-csv", "application/vnd.ms-excel", "text/plain",
+      "application/zip",
+      "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"]
+  }
+
+readFile(event: any) {
+    const files = event.target?.files;
+
+    if (files && files[0]) this.processFile(files[0]);
+
+  }
+
+  processFile(file: File) {
+    var reader = new FileReader();
+    reader.onload = (e: any) => {
+      var data = e.target.result;
+      var workbook = XLSX.read(data, {
+        type: 'binary'
+      });
+
+      workbook.SheetNames.forEach((sheetName) => {
+        var XL_row_object = XLSX.utils.sheet_to_json(workbook.Sheets[sheetName],);
+        var cols = this.get_header_row(workbook.Sheets[sheetName])
+        this.data = XL_row_object;
+        this.headers = cols;
+      })
+
+      reader.onloadend = () => {
+        this.$emit('onLoad', {data: this.data, headers: this.headers})
+      }
+
+    };
+
+    reader.onerror = function (ex) {
+      console.log(ex);
+    };
+
+    reader.readAsBinaryString(file);
+  }
+
+  get_header_row(sheet:any) {
+    var headers = [];
+    var range = XLSX.utils.decode_range(sheet['!ref']);
+    var C, R = range.s.r; /* start in the first row */
+    /* walk every column in the range */
+    for(C = range.s.c; C <= range.e.c; ++C) {
+      var cell = sheet[XLSX.utils.encode_cell({c:C, r:R})] /* find the cell in the first row */
+
+      var hdr = "UNKNOWN " + C;
+      if(cell && cell.t) hdr = XLSX.utils.format_cell(cell);
+
+      headers.push(hdr);
+    }
+    return headers;
+  }
+
+    
+  }
+
+</script>
+
+<style scoped lang="scss">
+  .vue-doc-reader {
+
+  }
+</style>
